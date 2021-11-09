6e226944e8812ce3f28240077d2675036084cd1a
Aaron Visser <avisser20@stuy.edu>
2021-11-09 17:35:05 -0500

    assignment

--- main.c
+++ main.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
 
+static void signhandler(int signo){
+  if(signo == SIGINT) {
+    printf("Exiting because of SIGINT");
+    exit(0);
+  } else if(signo == SIGUSR1){
+    printf("PPID: %d\n", getppid());
+  }
+}
+int main(){
+  while(1){
+    signal(SIGINT, signhandler);
+    printf("%d \n", getpid());
+    sleep(1);
+  }
+  return 0;
+}
--- makefile
+++ makefile
@@ -1,1 +1,13 @@
+all: main.o
+	gcc -o program main.o
+main.o: main.c
+	gcc -c main.c
 
+clean:
+	rm -rf program main.o
+
+run:
+	./program
+© 2021 GitHub, Inc.
+Terms
+Privacy
